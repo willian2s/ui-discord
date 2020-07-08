392b83ffe14e77dc863b4e26ebf1e365a573cfac
Willian Silva <silva.williansantos@gmail.com>
2020-07-08 18:35:03 -0300

    Start clone ui Discord

---
+++ src/components/ServerList/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import ServerButton from '../ServerButton';
+
+import { Container, Separator } from './styles';
+
+const ServerList: React.FC = () => {
+  return (
+    <Container>
+      <ServerButton isHome />
+
+      <Separator />
+
+      <ServerButton />
+      <ServerButton hasNotification />
+      <ServerButton mentions={3} />
+      <ServerButton />
+      <ServerButton />
+      <ServerButton />
+      <ServerButton />
+      <ServerButton />
+      <ServerButton />
+      <ServerButton />
+      <ServerButton />
+      <ServerButton />
+    </Container>
+  )
+};
+
+export default ServerList;
